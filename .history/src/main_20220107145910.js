cd97a6a4a25a4046d7b0ea9591164af9a7b66312
manhkkkk <pktopad@gmail.com>
2022-01-10 01:02:19 +0700

    update

---
+++ .history/src/main_20220107145910.js
@@ -0,0 +1,9 @@
+var menuList =["Menu1", "Menu2", "Menu3"];
+
+const menuElement = document.querySelector('#menu');
+if (menuElement) {
+  for (let i = 0; i < menuList.length; i++) {
+    menuElement.innerHTML += '<li>${menuList[i]}</li>';
+    
+  }
+}
